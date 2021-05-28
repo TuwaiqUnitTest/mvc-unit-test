795f1ce2d691285ada9d55dc3084254bb946daa5
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-05-28 17:55:31 +0300

    feat: added Movie and Review models

---
+++ MoviesApp/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MoviesApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string CoverURL { get; set; }
+        public int Rating { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ MoviesApp/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MoviesApp.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+        [JsonIgnore]
+        public Movie movie { get; set; }
+        public int MovieID { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}
