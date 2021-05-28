a2f8ae9113601f9def778bda6db432ebb0edf070
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-05-28 08:01:29 +0300

    refactor: removed action and enforced id type to be int into the default route pattern

--- MoviesApp/Startup.cs
+++ MoviesApp/Startup.cs
@@ -59,7 +59,7 @@ namespace MoviesApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                    pattern: "api/v1/{controller=Movies}/{id?:int}");
             });
 
             app.UseSpa(spa =>
