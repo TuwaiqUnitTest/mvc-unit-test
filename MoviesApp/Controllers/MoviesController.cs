0518e74cf1b1d9a09853c3b878b0a0ccdf422fb7
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-05-28 18:07:56 +0300

    feat: completed all controllers logic

--- MoviesApp/Controllers/MoviesController.cs
+++ MoviesApp/Controllers/MoviesController.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Text.Json;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApp.Data;
+using MoviesApp.Models;
 
 namespace MoviesApp.Controllers
 {
@@ -8,39 +11,66 @@ namespace MoviesApp.Controllers
     [Route("api/v1/[controller]")]
     public class MoviesController : ControllerBase
     {
+        private MoviesContext db { get; }
+
+        public MoviesController(MoviesContext db)
+        {
+            this.db = db;
+        }
+        
         // GET: api/v1/movies
         [HttpGet]
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return Ok();
+            return Ok(await this.db.Movies.Include(movie => movie.Reviews).ToListAsync());
         }
 
         // GET api/v1/movies/5
         [HttpGet("{id:int}")]
-        public ActionResult Show([FromRoute] int id)
+        public async Task<ActionResult> Show([FromRoute] int id)
         {
-            return Ok();
+            return Ok(await this.db.Movies.Include(movie => movie.Reviews).FirstOrDefaultAsync(movie => movie.ID == id));
         }
 
         // POST api/v1/movies
         [HttpPost]
-        public ActionResult Create()
+        public async Task<ActionResult> Create([FromBody] Movie movie)
         {
-            return Ok();
+            // TODO validate data
+            await this.db.Movies.AddAsync(movie);
+            await this.db.SaveChangesAsync();
+            return Ok(movie);
         }
 
         // PUT api/v1/movies/5
         [HttpPut("{id:int}")]
-        public ActionResult Update([FromRoute] int id)
+        public async Task<ActionResult> Update([FromRoute] int id, [FromBody] Movie input)
         {
-            return Ok();
+            // TODO validate data
+            Movie movie = await this.db.Movies.FirstOrDefaultAsync(movie => movie.ID == id);
+
+            if (movie == null)
+                return NotFound();
+
+            movie.Name = input.Name;
+            movie.CoverURL = input.CoverURL;
+            this.db.Movies.Update(movie);
+            await this.db.SaveChangesAsync();
+            return Ok(movie);
         }
 
         // DELETE api/v1/movies/5
         [HttpDelete("{id:int}")]
-        public ActionResult Delete([FromRoute] int id)
+        public async Task<ActionResult> Delete([FromRoute] int id)
         {
-            return Ok();
+            Movie movie = await this.db.Movies.FirstOrDefaultAsync(movie => movie.ID == id);
+            
+            if (movie == null)
+                return NotFound();
+            
+            this.db.Movies.Remove(movie);
+            await this.db.SaveChangesAsync();
+            return Ok(movie);
         }
     }
 }
--- MoviesApp/Controllers/ReviewsController.cs
+++ MoviesApp/Controllers/ReviewsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApp.Data;
+using MoviesApp.Models;
 
 namespace MoviesApp.Controllers
 {
@@ -6,39 +11,101 @@ namespace MoviesApp.Controllers
     [Route("api/v1/movies/{movieId:int}/[controller]")]
     public class ReviewsController : ControllerBase
     {
+        private MoviesContext db { get; }
+
+        public ReviewsController(MoviesContext db)
+        {
+            this.db = db;
+        }
+        
         // GET: api/v1/movies/1/reviews
         [HttpGet]
-        public ActionResult Index([FromRoute] int movieId)
+        public async Task<IActionResult> Index([FromRoute] int movieId)
         {
-            return Ok();
+            Movie movie = await this.db.Movies.Include(movie => movie.Reviews).FirstOrDefaultAsync(movie => movie.ID == movieId);
+
+            if (movie == null)
+                return NotFound();
+            
+            return Ok(movie.Reviews.ToList());
         }
 
         // GET api/v1/movies/1/reviews/5
         [HttpGet("{id:int}")]
-        public ActionResult Show([FromRoute] int movieId, [FromRoute] int id)
+        public async Task<IActionResult> Show([FromRoute] int movieId, [FromRoute] int id)
         {
-            return Ok();
+            Movie movie = await this.db.Movies.Include(movie => movie.Reviews).FirstOrDefaultAsync(movie => movie.ID == movieId);
+
+            if (movie == null)
+                return NotFound();
+
+            Review review = movie.Reviews.FirstOrDefault(review => review.ID == id);
+
+            if (review == null)
+                return NotFound();
+            
+            return Ok(review);
         }
 
         // POST api/v1/movies/1/reviews
         [HttpPost]
-        public ActionResult Create([FromRoute] int movieId)
+        public async Task<IActionResult> Create([FromRoute] int movieId, [FromBody] Review input)
         {
-            return Ok();
+            // TODO validate data
+            Movie movie = await this.db.Movies.Include(movie => movie.Reviews).FirstOrDefaultAsync(movie => movie.ID == movieId);
+
+            if (movie == null)
+                return NotFound();
+
+            var review = new Review();
+            review.Author = input.Author;
+            review.Content = input.Content;
+            
+            movie.Reviews.Add(review);
+            this.db.Movies.Update(movie);
+            await this.db.SaveChangesAsync();
+            return Ok(review);
         }
 
         // PUT api/v1/movies/1/reviews/5
         [HttpPut("{id:int}")]
-        public ActionResult Update([FromRoute] int movieId, [FromRoute] int id)
+        public async Task<IActionResult> Update([FromRoute] int movieId, [FromRoute] int id, [FromBody] Review input)
         {
-            return Ok();
+            // TODO validate data
+            Movie movie = await this.db.Movies.Include(movie => movie.Reviews).FirstOrDefaultAsync(movie => movie.ID == movieId);
+
+            if (movie == null)
+                return NotFound();
+
+            Review review = movie.Reviews.FirstOrDefault(review => review.ID == id);
+
+            if (review == null)
+                return NotFound();
+
+            review.Author = input.Author;
+            review.Content = input.Content;
+            this.db.Reviews.Update(review);
+            await this.db.SaveChangesAsync();
+            return Ok(review);
         }
 
         // DELETE api/v1/movies/1/reviews/5
         [HttpDelete("{id:int}")]
-        public ActionResult Delete([FromRoute] int movieId, [FromRoute] int id)
+        public async Task<IActionResult> Delete([FromRoute] int movieId, [FromRoute] int id)
         {
-            return Ok();
+            Movie movie = await this.db.Movies.Include(movie => movie.Reviews).FirstOrDefaultAsync(movie => movie.ID == movieId);
+            
+            if (movie == null)
+                return NotFound();
+
+            Review review = movie.Reviews.FirstOrDefault(review => review.ID == id);
+
+            if (review == null)
+                return NotFound();
+            
+            movie.Reviews.Remove(review);
+            await this.db.SaveChangesAsync();
+            return Ok(review);
         }
     }
 }
