ed841eaf1baa94c1d4047199098b780323316d37
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-05-28 08:04:45 +0300

    chore: added MoviesController.cs

---
+++ MoviesApp/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesApp.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class MoviesController : Controller
+    {
+        // GET: api/v1/movies
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok();
+        }
+
+        // GET api/v1/movies/5
+        [HttpGet("{id}")]
+        public ActionResult Show([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        // POST api/v1/movies
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return Ok();
+        }
+
+        // PUT api/v1/movies/5
+        [HttpPut("{id}")]
+        public ActionResult Update([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        // DELETE api/v1/movies/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            return Ok();
+        }
+    }
+}
