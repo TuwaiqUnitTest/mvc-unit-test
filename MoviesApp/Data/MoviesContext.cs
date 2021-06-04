1c08a67cb337665512b31bd6611189d800ffb211
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-06-05 01:44:11 +0300

    feat: seeding movies table

--- MoviesApp/Data/MoviesContext.cs
+++ MoviesApp/Data/MoviesContext.cs
@@ -5,13 +5,35 @@ namespace MoviesApp.Data
 {
     public class MoviesContext : DbContext
     {
-        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options)
-        {
-        }
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options) {}
         
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Cast> Casts { get; set; }
         public DbSet<Actor> Actors { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            GenerateMovies(10, modelBuilder);
+        }
+
+        private void GenerateMovies(int amount, ModelBuilder modelBuilder)
+        {
+            for (var i = 1; i <= amount; i++)
+            {
+                CreateMovie(new Movie
+                {
+                    ID = i,
+                    Name = Faker.CompanyFaker.Name(),
+                    CoverURL = "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg",
+                    Rating = Faker.NumberFaker.Number(2, 10)
+                }, modelBuilder);
+            }
+        }
+        
+        private void CreateMovie(Movie movie, ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(movie);
+        }
     }
 }
---
+++ MoviesApp/Migrations/20210604224248_SeedingMoviesTable.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesApp.Migrations
+{
+    public partial class SeedingMoviesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "ID", "CoverURL", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Davis LLC", 4 },
+                    { 2, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Rodriguez Inc", 6 },
+                    { 3, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Oates Inc", 6 },
+                    { 4, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Jackson Group", 7 },
+                    { 5, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Edwards and Sons", 2 },
+                    { 6, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Robertson LLC", 3 },
+                    { 7, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Peterson LLC", 9 },
+                    { 8, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Kirby LLC", 5 },
+                    { 9, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Burrows LLC", 7 },
+                    { 10, "https://i.pinimg.com/originals/c3/be/67/c3be67bf37cbd41fc762a3254e3ed6cd.jpg", "Thomas LLC", 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "ID",
+                keyValue: 10);
+        }
+    }
+}
