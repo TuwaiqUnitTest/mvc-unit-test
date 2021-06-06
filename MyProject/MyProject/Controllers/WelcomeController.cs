6c8d045f4ed3f72130854a2b1cbb1e17afb39a4f
Ahmed <dev@test.com>
2021-06-06 19:49:12 +0300

    Done

---
+++ MyProject/MyProject/Controllers/WelcomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyProject.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: /<controller>/
+        public string ConcatString(string str1 ,string str2)
+        {
+            if (str1 == null || str2 == null)
+                return null;
+
+
+            
+            return str1+ str2;
+        }
+
+        public int sum(int FirstNumber, int SecondNumber)
+        {
+            
+            return FirstNumber + SecondNumber;
+        }
+
+        public int subtraction(int FirstNumber, int SecondNumber)
+        {
+
+            return FirstNumber - SecondNumber;
+        }
+    }
+}
---
+++ MyProject/MyProjectTests/UnitTest1.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyProject.Controllers;
+namespace MyProjectTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConcatStringTestMethod()
+        {
+
+            var controller = new WelcomeController();
+            var actual = controller.ConcatString("Hello ","World");
+          
+            string  expected = "Hello World";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ConcatStringTestMethod_Null_Value()
+        {
+
+            var controller = new WelcomeController();
+            var actual = controller.ConcatString(null, "World");
+            
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void SumTestMethod()
+        {
+
+            var controller = new WelcomeController();
+            var actual = controller.sum(3, 3);
+
+            int expected = 6;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void subtractionTestMethod()
+        {
+
+            var controller = new WelcomeController();
+            var actual = controller.subtraction(3, 3);
+
+            int expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
