27ad3a9bae19877a8e606161c9138684a44c7c51
Faisal Abdulaziz <faisal.alsagri.1@gmail.com>
2021-06-06 13:26:33 +0300

    (⓿_⓿)

---
+++ Calculator/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int Sum(int x, int y) => (x + y);
+        public int Sub(int x, int y) => (x - y);
+        public int Div(int x, int y) => y == 0 ? throw new DivideByZeroException("Cannot divide by zero") : (x / y);
+
+    }
+}
---
+++ Calculator/CalculatorTest/Controllers/CalculatorControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using Calculator.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CalculatorTest.Controllers
+{
+    [TestClass]
+    public class CalculatorControllerTest
+    {
+
+
+        [TestMethod]
+        public void SumTest()
+        {
+            //Arange
+
+            CalculatorController OBJ = new CalculatorController();
+
+            //Act
+
+            int test1 = OBJ.Sum(4, 4);
+            int test2 = OBJ.Sum(4, -0);
+
+            //Assert
+
+            Assert.AreEqual(8, test1);
+            Assert.AreEqual(4, test2);
+
+
+        }
+
+        [TestMethod]
+        public void SubTest()
+        {
+            //Arange 
+            CalculatorController OBJ = new CalculatorController();
+
+            //Act
+
+            int test1 = OBJ.Sub(4, 2);
+            int test2 = OBJ.Sub(4, -3);
+
+            //Assert
+
+            Assert.AreEqual(2, test1);
+            Assert.AreEqual(7, test2);
+
+
+        }
+
+        [TestMethod]
+        public void DivTest()
+        {
+            //Arange
+
+            CalculatorController OBJ = new CalculatorController();
+
+            //Act
+
+            int test1 = OBJ.Div(8, 2);
+            int test2 = OBJ.Div(4, -2);
+
+            //Assert
+
+            Assert.AreEqual(4, test1);
+            Assert.AreEqual(-2, test2);
+            Assert.ThrowsException<DivideByZeroException>(() => OBJ.Div(2, 0));
+
+
+        }
+    }
+}
