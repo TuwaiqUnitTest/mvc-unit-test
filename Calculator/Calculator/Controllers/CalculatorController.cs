a017d40d1936c9f1efd81e7f7c683650aabcd843
SamirahAlhusayni <samirah.alhusayni@tuwaiq.edu.sa>
2021-06-06 17:50:11 +0300

    MVC Calculator Testing

---
+++ Calculator/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public int Add(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+        public int Sub(int param1, int param2)
+        {
+            return param1 - param2;
+        }
+        public int Mul(int param1, int param2)
+        {
+            return param1 * param2;
+        }
+        public int Div(int param1, int param2)
+        {
+            if (param2 == 0)
+                throw new DivideByZeroException("Can't divide by zero, Otherwise you will got infinity");
+            else
+                return param1 / param2;
+        }
+    }
+}
+
+
---
+++ Calculator/CalculatorTest/Calculator/CalculatorTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.Controllers;
+
+namespace CalculatorTest.Calculator
+{
+    class CalculatorTest
+    {
+        [TestMethod]
+        public void Addition()
+        {
+            //Arange
+            CalculatorController cal = new CalculatorController();
+
+            //Act
+            int result = cal.Add(5, 10);
+            //Assert
+            Assert.AreEqual(15, result);
+          
+        }
+        [TestMethod]
+        public void Subtraction()
+        {
+            //Arange
+            CalculatorController cal = new CalculatorController();
+
+            //Act
+            int result = cal.Sub(200, 100);
+            //Assert
+            Assert.AreEqual(100, result);
+    
+        }
+        [TestMethod]
+        public void Multiplication()
+        {
+            //Arange
+            CalculatorController cal = new CalculatorController();
+
+            //Act
+            int result = cal.Mul(4, 100);
+       
+
+            //Assert
+            Assert.AreEqual(400, result);
+         
+        }
+        [TestMethod]
+        public void Division()
+        {
+            //Arange
+            CalculatorController cal = new CalculatorController();
+
+            //Act
+            int result = cal.Div(18, 6);
+         
+
+            //Assert
+            Assert.AreEqual(3, result);
+        
+        }
+    }
+}
+
+
+
+
