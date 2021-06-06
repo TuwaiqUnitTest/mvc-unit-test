8c8deb8b08acd9811c7fa77ba44837a9b4a6b3e5
Albandry AlQaseemi <Albandr.alqaseemi@gmail.com>
2021-06-06 17:15:52 +0300

    commit files

---
+++ Calculator/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        float result;
+        public float Sum(float FirstNum, float SecondNum)
+        {
+            return result = 4 + 6;
+        }
+        public float Sub(float FirstNum, float SecondNum)
+        {
+            return result = 6 - 3;
+        }
+        public float Mul(float FirstNum, float SecondNum)
+        {
+            return result = 2 * 3;
+        }
+    }
+}
---
+++ Calculator/CalculatorTest/Controllers/CalculatorControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.Controllers;
+
+namespace CalculatorTest.Controllers
+{
+    [TestFixture]
+    class CalculatorControllerTest
+    {
+        [Test]
+        public void TestSumMethod()
+        {
+
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+
+            //Act
+            float result = calc.Sum(9, 1);
+
+            //Assert
+            Assert.AreEqual(10, result);
+        }
+        [Test]
+        public void TestSubMethod()
+        {
+
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+
+            //Act
+            float result = calc.Sub(6, 3);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+        [Test]
+        public void TestMulMethod()
+        {
+
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+
+            //Act
+            float result = calc.Mul(3, 2);
+
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
